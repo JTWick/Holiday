c61bc4e65c5a9f09f03a1513f1956593c56bb25d
barwick.jared <barwick.jared@vashonsd.org>
2022-04-25 11:28:13 -0700

    Added more holidays

--- src/org/vashonsd/Main.java
+++ src/org/vashonsd/Main.java
@@ -5,5 +5,9 @@ public class Main {
     public static void main(String[] args) {
 	Holiday h = new Holiday(4, "The Day Concert", "A concert delving deep into the world's problems", true);
         System.out.println(h);
+        Holiday t = new Holiday(5, "Later", "Concert about words", false);
+        System.out.println(t);
+        Holiday p = new Holiday(9, "Never", "Sadness", true);
+        System.out.println(p);
     }
 }
